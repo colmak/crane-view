edcef2ccd2517a9317e73b5eb77763cf9593683f
colmak <69098858+colmak@users.noreply.github.com>
2024-11-04 20:55:22 -0500

    simple demo spinning box

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import './App.css';
 
+function Box() {
+  const mesh = useRef<THREE.Mesh>(null!);
 
-function App() {
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.01;
+    }
+  });
 
   return (
-    <>
-
-    </>
-  )
+    <mesh ref={mesh} position={[0, 0, 0]}>
+      <boxGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial color={'orange'} />
+    </mesh>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box />
+    </Canvas>
+  );
+}
+
+export default App;
